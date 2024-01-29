f0bab956b4fc9826b860cff34ed0b884bb26ddfd
magikcoco <magikcoco.dev@gmail.com>
2024-01-29 17:34:59 -0500

    stock-ticker-list.py now downloads stock info from https://eodhd.com/ and prints it

--- launch.py
+++ launch.py
@@ -10,6 +10,8 @@ def enter_venv():
     virtual_env_path = "./venv/"
     virtual_env_python = os.path.join(virtual_env_path, "bin", "python3")
     os.execl(virtual_env_python, virtual_env_python, *sys.argv)
+    #TODO: handle error when venv does not exist by creating one
+    #TODO: handle permission error as fatal
      
 def launch():
     print("Running: ", __file__)
@@ -21,3 +23,7 @@ def launch():
         enter_venv()
 
 launch()
+
+#TODO: Check dependencies of all scripts, install missing modules (prompt user for permission if the script didnt create its own virtual environment)
+#TODO: Check if the correct version of python is running. If not, offer user to install it.
+#TODO: Flags for enabling/disabling venv check, version check, module check, and turn them off by default
--- stock-ticker-list.py
+++ stock-ticker-list.py
@@ -1,18 +1,30 @@
 import sys
+import json
+import pandas as pd
+import requests
 
 print("Running: ", __file__)
 print("In: ", sys.prefix)
-print("stock tickers!")
-""" from yahooquery import Ticker
 
-ticker_list = ["GOOG", "AMZN", "SAVE"]
-all_symbols = " ".join(ticker_list)
-myInfo = Ticker(all_symbols)
-myDict = myInfo.price
+def get_exchange_data(key, exchange='NYSE'):
+    """
+    returns metadata for specific exchange
+    available: US, NASDAQ, OTCBB, PINK, BATS
+    """
+    endpoint = f"https://eodhistoricaldata.com/api/exchange-symbol-list/"
+    endpoint += f"{exchange}?api_token={key}&fmt=json"
+    print("Downloading stock data...")
+    call = requests.get(endpoint).text
+    exchange_data = pd.DataFrame(json.loads(call))
+    print("Finished!")
+    return exchange_data
 
-for ticker in ticker_list:
-    ticker = str(ticker)
-    longName = myDict[ticker]['longName']
-    market_cap = myDict[ticker]['marketCap']
-    price = myDict[ticker]['regularMarketPrice']
-    print(ticker, longName, market_cap, price) """
+def main():
+    key = open('api_token.txt').read()
+    print(get_exchange_data(key))
+
+if __name__ == '__main__':
+    main()
+
+# TODO: iterate over each exchange
+# TODO: output into json file named [exchange].json
